98483effaa8e893ea6d2fca9e14a761db08c15ce
sengarsingh <149374164+sengarsingh@users.noreply.github.com>
2024-06-27 12:53:33 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "ENTER API ID"
-api_hash = "ENTER API HASH"
-bot_token = "ENTER BOT TOKEN"
+api_id = "25912801"
+api_hash = "f14e8717578935103e2774559184a345"
+bot_token = "7204500473:AAGSACsn0_dyjyp7h-VJ6iBifA_DogNI7Qw"
